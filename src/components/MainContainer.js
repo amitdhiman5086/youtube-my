d36dee4ed1370164305b30d015fc94234f96d323
Amit <amitdhiman5086@gmail.com>
2024-06-17 17:24:20 +0530

    ok

---
+++ src/components/MainContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Videolist from './Videolist'
+import ButtonList from './ButtonList'
+
+const MainContainer = () => {
+  return (
+    <div>
+        <Videolist/>
+        <ButtonList/>
+    </div>
+  )
+}
+
+export default MainContainer
---
+++ src/components/Videolist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Videolist = () => {
+  return (
+    <div>Videolist</div>
+  )
+}
+
+export default Videolist
